aa76057d1daafa9b8bbb8cc47d79b3ccf7bd9419
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-16 10:20:23 +0530

    intermediate 1

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const port  = 3000;
 app.set('view engine','ejs');
 app.set("views",path.join(__dirname,'/views'));
 
+app.get('/ig/:username',(req,res)=>{
+    let {username}= req.params;
+    res.render("instagram.ejs",{username})
+})
+
 app.get('/',(req,res)=>{
-    res.render("instagram.ejs");
+    res.send("use url {/ig/username} to render the page!");
 })
 
 app.listen(port,()=>{
