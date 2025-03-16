0d9fd7bb5c68834f14498751b980d3f195b2c400
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-16 11:34:19 +0530

    done

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const path = require('path');
 const app = express();
 const port  = 3000;
 
+
+app.use(express.static('public'))
+
 app.set('view engine','ejs');
 app.set("views",path.join(__dirname,'/views'));
 
 app.get('/ig/:username',(req,res)=>{
-    let {username}= req.params;
-    res.render("instagram.ejs",{username})
+    const instaData = require('./data.json');
+    let {username} = req.params;
+    let data = instaData[username]
+    if(data){
+        res.render("instagram.ejs",{username,data})
+    }
+    else{
+        res.render("error.ejs")
+    }
 })
 
 app.get('/',(req,res)=>{
